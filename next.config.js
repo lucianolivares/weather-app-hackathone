53c8400b44782100246ba5cb988d6447e3e5567b
lucianolivares <lucianolivares.lo@gmail.com>
2022-05-25 23:55:22 -0400

    using the api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["cdn.weatherapi.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["https://cdn.weatherapi.com/weather/64x64/night/113.png"],
+  },
+};
